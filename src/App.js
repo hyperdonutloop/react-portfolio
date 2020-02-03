8b879c4e809764f8035f2b55da888c6224ecadf7
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-03 11:50:32 -0800

    components set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import About from './components/About.js';
+import Projects from './components/Projects.js';
+import Contact from './components/Contact.js';
+import Home from './components/Home';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        
+        {/* Setting up routes */}
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component ={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component ={Contact} />
+        
+
+        <div className="navigation">
+          <img src={logo} className="logo" alt="Logo Image" />
+          <div className="navigation-sub">
+
+            {/* Setting up links */}
+            <a href="" className="item">About</a>
+            <a href="" className="item">Projects</a>
+            <a href="" className="item">Contact</a>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
