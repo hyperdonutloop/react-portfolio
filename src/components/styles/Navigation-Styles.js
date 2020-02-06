ee2bc482992b7533fc956b670a578dc522afc9f5
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-06 09:22:54 -0800

    separated navigation styles from main component

---
+++ src/components/styles/Navigation-Styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  .navigation {
+    display: flex;
+    justify-content: flex-end;
+    padding-top: 2%;
+    padding-bottom: 1%;
+    // border: 2px dotted pink;
+
+    .links a {
+      padding: 1rem;
+      text-decoration: none;
+      color: #e2e8f0;
+      // margin: 1rem;
+      border: 2px solid blue;
+    }
+  }
+`;
