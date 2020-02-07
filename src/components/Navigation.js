8a6398fba22d4d5ec11a983c85d5d975ab89990c
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-06 22:53:56 -0800

    added switch but it is not fully functional yet

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,6 +13,14 @@ const Navigation = () => {
           <Link to="/about" className="item">About</Link>
           <Link to="/projects" className="item">Projects</Link>
         </div>
+        <div class="switch-wrapper">
+          <div class="sun"></div>
+          <div class="toggle-wrapper">
+          <input id="switch" type="checkbox" />
+          <label for="switch" id="toggle">Toggle</label>
+          </div>
+          <div class="moon"></div>
+        </div>
       </div>
     </Wrapper>
     
