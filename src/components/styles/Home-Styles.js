a26612d4ac9ac74a38bf038cac7bd2c2453f87db
donutwizard666 <thebeardedwarrior@icloud.com>
2020-04-23 22:40:53 -0700

    connected to iphone on localhost and fixed mobile view for home page

--- src/components/styles/Home-Styles.js
+++ src/components/styles/Home-Styles.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
     margin-right: auto;
     margin-top: 10rem;
     max-width: 1024px;
-    // width: 100%;
+    /* width: 100%; */
     padding-top: 3rem;
     padding-bottom: 3rem;
     padding: 1rem;
@@ -14,8 +14,10 @@ import styled from 'styled-components';
     display: flex;
     flex-direction: row;
 
+    
 
     .main-font {
+
 
       span.wave {
         animation-name: wave-animation;    /* Refers to the name of your @keyframes element below */
@@ -25,40 +27,29 @@ import styled from 'styled-components';
         display: inline-block;
       }
 
-        @keyframes wave-animation {
-          0% { transform: rotate(  0.0deg) }
-          10% { transform: rotate(-10.0deg) }    
-          20% { transform: rotate( 12.0deg) }
-          30% { transform: rotate(-10.0deg) }
-          40% { transform: rotate(  9.0deg) }
-          50% { transform: rotate(  0.0deg) }    /* Reset for the last half to pause */
-          100% { transform: rotate(  0.0deg) }
-        }
+      @keyframes wave-animation {
+        0% { transform: rotate(  0.0deg) }
+        10% { transform: rotate(-10.0deg) }    
+        20% { transform: rotate( 12.0deg) }
+        30% { transform: rotate(-10.0deg) }
+        40% { transform: rotate(  9.0deg) }
+        50% { transform: rotate(  0.0deg) }    /* Reset for the last half to pause */
+        100% { transform: rotate(  0.0deg) }
+      }
      
       margin-top: 4rem;
       margin-bottom: 4rem;
-      /* border: 3px solid red; */
+      border: 3px solid red;
       font-size: 2.50rem;
       /* color: #FFFFFF; */
       /* margin-left: 2rem; */
 
-      div.motion {
-        /* max-width: 20%; */
-        /* border: 2px solid green; */
-      }
-
-      @media screen and (max-width: 1055px) {
-        font-size: 2.30rem;
-      }
-
-      @media screen and (max-width: 375px) {
-        font-size: 1em;
-        /* margin-left: 2rem; */
-        border: 2px dotted green;
-        /* text-align: left; */
-        /* width: 400px; */
-        margin-bottom: 2rem;
-        /* max-width: 100%; */
+      @media screen and (max-width: 500px) {
+        margin-top: 1rem;
+        /* border: 2px dotted green; */
+        font-size: 1rem;
+        margin-bottom: 1.5rem;
+        margin-top: 3rem;
       }
 
     }
@@ -71,7 +62,7 @@ import styled from 'styled-components';
       padding-right: 0rem;
       margin-top: 2rem;
       margin-left: 3rem;
-      /* border: 2px dotted blue; */
+      border: 2px dotted blue;
       
       @media screen and (max-width: 800px) {
         /* border: 2px dashed purple; */
@@ -82,7 +73,12 @@ import styled from 'styled-components';
         border: 2px dashed purple;
         width: 100px;
         margin-left: 0;
-        padding-left: 15px;
+        padding-left: 2rem;
+
+        /* padding-right: 0rem;
+        margin-top: 0rem;
+        margin-left: 0rem; */
+        /* padding-left: 15px; */
         /* padding-right: 2rem; */
         
         /* padding-top: 50px; */
@@ -132,7 +128,7 @@ import styled from 'styled-components';
     @media screen and (max-width: 500px) {
         font-size: 0.8rem;
         letter-spacing: normal;
-        /* border: 1px dashed yellow; */
+        border: 2px dashed red;
         /* max-width: 100%;
         overflow-y: auto; */
         
