996d665632a0c8acf9bf60592eef69b8ca52c466
donutwizard666 <thebeardedwarrior@icloud.com>
2020-04-23 23:35:39 -0700

    finish nav bar and then work on about page next

--- src/components/styles/Navigation-Styles.js
+++ src/components/styles/Navigation-Styles.js
@@ -154,47 +154,12 @@ export const Wrapper = styled.div`
         /* border: 1px dotted papayawhip; */
       }
     
-    /* .toggle-wrapper {
-      margin: 10px;
-      border: 3px solid purple;
-      #switch {
-        height: 0;
-        width: 0;
-        visibility: hidden;
-        position: absolute;
-        &:checked + label {
-          background: $green;
-        }
-        &:checked + label:after {
-          left: calc(100% - 3px);
-          transform: translateX(-100%);
-        }
-      }
-
-        label {
-          cursor: pointer;
-          text-indent: -9999px;
-          width: 45px;
-          height: 25px;
-          background: grey;
-          display: block;
-          border-radius: 100px;
-          
-          position: relative;
-          &:after {
-            @include transition;
-            content: '';
-            position: absolute;
-            top: 3px;
-            left: 3px;
-            width: 19px;
-            height: 19px;
-            background: #fff;
-            border-radius: 100%;
-          }
-        }
-  } */
 } /* toggle-wrapper closing tag*/
+
+@media screen and (max-width: 500px) {
+        border: 2px dashed green;
+        font-size: 1rem;
+      }
 
 }
 `;
